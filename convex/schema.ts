cac1b7d4b6a21db413e59d80a5405bd1bf6d3c61
Brian Kidd <brian.kidd.one@gmail.com>
2025-03-26 23:09:35 -0700

    adds schema definitions for products, sales, and keys tables and also adds index helper functions to make querying the tables more efficient

--- convex/schema.ts
+++ convex/schema.ts
@@ -10,5 +10,34 @@ export default defineSchema({
     email: v.string(),
     about: v.optional(v.string()),
     logo: v.optional(v.string()),
-  }),
+  })
+    // indexing makes it more efficient to find data entries because like a card catalog in a library, it can index certain data to make it easier to find. This prvents us from having to search through the entire database.
+    .index("by_clerkId", ["clerkId"])
+    .index("by_username", ["username"]),
+  products: defineTable({
+    clerkId: v.string(),
+    name: v.string(),
+    description: v.string(),
+    content: v.optional(v.string()),
+    price: v.number(),
+    // we want to store the price in this products table and the sales table in case the store owner changes the price we do not want to change any sale records, which could have the old price
+    currency: v.string(),
+    coverImage: v.optional(v.string()),
+    published: v.boolean(),
+  }).index("by_clerkId", ["clerkId"]),
+  sales: defineTable({
+    storeClerkId: v.string(),
+    customerClerkId: v.string(),
+    productId: v.string(),
+    price: v.number(),
+    currency: v.string(),
+  })
+    .index("by_storeClerkId", ["storeClerkId"])
+    .index("by_customerClerkId", ["customerClerkId"])
+    .index("by_productId", ["productId"]),
+  // Stripe keys
+  keys: defineTable({
+    clerkId: v.string(),
+    stripeKey: v.string(),
+  }).index("by_clerkId", ["clerkId"]),
 });
--- convex/utils.ts
+++ convex/utils.ts
@@ -5,9 +5,8 @@ import {
   customMutation,
   customQuery,
 } from "convex-helpers/server/customFunctions";
-import { action, mutation, query } from "./_generated/server";
-import { ConvexError } from "convex/values";
-
+import { action, DatabaseReader, mutation, query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
 async function getUserId(ctx: { auth: Auth }) {
   // context (ctx) has information about our user including the userId
   const authInfo = await ctx.auth.getUserIdentity();
@@ -47,3 +46,94 @@ export const actionWithUser = customAction(
     return { userId };
   })
 );
+
+queryWithUser({
+  args: {
+    clerkId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    //     const user = await ctx.db
+    //       .query("users")
+    //       .withIndex("by_clerkId", (q) => q.eq("clerkId", args.clerkId))
+    //       .first();
+    //   },
+    const user = await getUserByClerkId(ctx.db, args.clerkId);
+  },
+});
+
+export const getUserByClerkId = async (db: DatabaseReader, clerkId: string) => {
+  const user = await db
+    .query("users")
+    .withIndex("by_clerkId", (q) => q.eq("clerkId", clerkId))
+    .first();
+  return user;
+};
+
+export const getUserByUsername = async (
+  db: DatabaseReader,
+  username: string
+) => {
+  const user = await db
+    .query("users")
+    .withIndex("by_username", (q) => q.eq("username", username))
+    .first();
+  return user;
+};
+
+export const getProductsByClerkId = async (
+  db: DatabaseReader,
+  clerkId: string
+) => {
+  const products = await db
+    // we use the collect method so that we can get an array of products
+    .query("products")
+    .withIndex("by_clerkId", (q) => q.eq("clerkId", clerkId))
+    .collect();
+  return products;
+};
+
+export const getSalesByStoreClerkId = async (
+  db: DatabaseReader,
+  storeClerkId: string
+) => {
+  const sales = await db
+    // we use the collect method so that we can get an array of products
+    .query("sales")
+    .withIndex("by_storeClerkId", (q) => q.eq("storeClerkId", storeClerkId))
+    .collect();
+  return sales;
+};
+export const getSalesByProductId = async (
+  db: DatabaseReader,
+  productId: string
+) => {
+  const sales = await db
+    // we use the collect method so that we can get an array of products
+    .query("sales")
+    .withIndex("by_productId", (q) => q.eq("productId", productId))
+    .collect();
+  return sales;
+};
+
+export const getSalesByCustomerClerkId = async (
+  db: DatabaseReader,
+  customerClerkId: string
+) => {
+  const sales = await db
+    // we use the collect method so that we can get an array of products
+    .query("sales")
+    .withIndex("by_customerClerkId", (q) =>
+      q.eq("customerClerkId", customerClerkId)
+    )
+    .collect();
+  return sales;
+};
+
+export const getKeyByClerkId = async (db: DatabaseReader, clerkId: string) => {
+  const key = await db
+    // we use the collect method so that we can get an array of products
+    .query("keys")
+    .withIndex("by_clerkId", (q) => q.eq("clerkId", clerkId))
+    .first();
+  return key;
+};
